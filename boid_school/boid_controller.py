39b82868bf924b1507320a0026ef965621e7568c
Nathan Snarr <natethegreat209@hotmail.com>
2026-06-26 11:06:03 -0700

    Initial BoidController class

---
+++ boid_school/boid_controller.py
@@ -0,0 +1,16 @@
+# Boid Controller
+from pygame import Surface
+from .boid import Boid
+
+class BoidController:
+    def __init__(self):
+        """Manages and updates boids."""
+        pass
+
+    def update(self, dt: float = 0):
+        """Updates all boid positions and angles."""
+        pass
+
+    def draw(self, surface: Surface):
+        """Draws boids to the surface."""
+        pass
