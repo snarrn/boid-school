41423eacfd8f33a2c590c68786fc27b2b7c7201d
Nathan Snarr <natethegreat209@hotmail.com>
2026-06-26 11:05:37 -0700

    Initial Boid class

---
+++ boid_school/boid.py
@@ -0,0 +1,21 @@
+# Boid
+
+class Boid:
+    def __init__(self):
+        """Boid that behaves based on alignment, cohesion, and separation."""
+        pass
+
+    def get_alignment_angle(self):
+        """Records and returns the alignments angle."""
+        pass
+
+    def get_cohesion_angle(self):
+        """Records and returns the cohesion angle."""
+    
+    def get_separation_angle(self):
+        """Records and returns the separation angle."""
+        pass
+
+    def move(self, dt: float = 0):
+        """Moves the boid, given a change in time."""
+        pass
