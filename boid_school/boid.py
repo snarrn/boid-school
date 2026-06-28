ec3c65f4bb8bf12298ef5d05e1f7d1c4a1cc924e
Nathan Snarr <natethegreat209@hotmail.com>
2026-06-28 14:21:43 -0700

    Moved two static functions to utilities

--- boid_school/boid.py
+++ boid_school/boid.py
@@ -2,6 +2,7 @@
 from pygame import Vector2, Surface, draw
 from typing import Sequence
 import math
+from utilities import get_avg_angle
 
 class Boid:
     """Boid that behaves based on alignment, cohesion, and separation."""
@@ -44,40 +45,6 @@ class Boid:
             (math.cos(angle+2.55)*.6,math.sin(angle+2.55)*.6),\
             (math.cos(angle-2.55)*.6,math.sin(angle-2.55)*.6))
 
-    @staticmethod
-    def get_avg_vector_angle(vectors: Sequence[Vector2], weights: Sequence[float] | None = None, normalise_vectors: bool = True):
-        """Returns the [weighted] average angle of the vectors."""
-        # Type Checking
-        if not isinstance(vectors, (tuple, list)):
-            raise TypeError(f"'vectors' must be a tuple or list, not {type(vectors)}")
-        if len(vectors) <= 0:
-            raise ValueError(f"'vectors' cannnot be an empty sequence.")
-        if weights is not None and len(weights) != len(vectors):
-            raise ValueError(f"'weights' must be the same length as 'vectors' ({len(weights)} != {len(vectors)})")
-
-        # Normalising Vectors
-        if normalise_vectors:
-            vectors = tuple((vector.normalize() if vector.magnitude != 0 else vector) for vector in vectors)
-        
-        # Average Calculation
-        total_vector = Vector2()
-        if weights is None:
-
-            for vector in vectors:
-                total_vector += vector
-        
-        else:
-            for vector, weight in zip(vectors, weights):
-                total_vector += vector * weight
-        
-        return math.atan2(total_vector.y, total_vector.x)
-
-    @staticmethod
-    def get_avg_angle(angles: Sequence[float], weights: Sequence[float] | None = None):
-        """Returns the [weighted] average angle."""
-        vectors = tuple((Vector2(math.cos(angle), math.sin(angle))) for angle in angles)
-        return Boid.get_avg_vector_angle(vectors, weights=weights, normalise_vectors=False)
-
     def clear_proximal_boids(self):
         """Clears the list of proximal boids."""
         self.proximal_boids = []
@@ -89,7 +56,7 @@ class Boid:
         
         elif len(self.proximal_boids) > 1:
             proximal_boid_angles = tuple((boid.angle) for boid in self.proximal_boids)
-            return Boid.get_avg_angle(proximal_boid_angles)
+            return get_avg_angle(proximal_boid_angles)
 
         return self.angle
 
@@ -126,10 +93,10 @@ class Boid:
         self.__target_cohesion_angle = self.get_cohesion_angle()
         self.__target_separation_angle = self.get_separation_angle()
 
-        self.__target_angle = Boid.get_avg_angle((self.__target_alignment_angle, self.__target_cohesion_angle, self.__target_separation_angle),
+        self.__target_angle = get_avg_angle((self.__target_alignment_angle, self.__target_cohesion_angle, self.__target_separation_angle),
                                          weights=(Boid.ALIGNMENT_WEIGHT, Boid.COHESION_WEIGHT, Boid.SEPARATION_WEIGHT))
 
-        self.angle = Boid.get_avg_angle((self.angle, self.__target_angle), weights=(Boid.CURRENT_ANGLE_WEIGHT, 1))
+        self.angle = get_avg_angle((self.angle, self.__target_angle), weights=(Boid.CURRENT_ANGLE_WEIGHT, 1))
 
     def update_position(self, dt: float = 0):
         """Moves the boid's position."""
