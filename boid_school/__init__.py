c9ee1b52953b11433eb789e438738c5b2a6a2f71
Nathan Snarr <natethegreat209@hotmail.com>
2026-06-28 14:53:01 -0700

    Added utilities to __all__

--- boid_school/__init__.py
+++ boid_school/__init__.py
@@ -4,11 +4,13 @@ Boid School
 A boid herding game built around Pygame.
 """
 
+from .utilities import *
 from .boid import Boid
 from .boid_controller import BoidController
 from .player import Player
 
-__all__ = ["Boid",
+__all__ = ["utilities",
+           "Boid",
            "BoidController",
            "Player",
            "__version__"]
