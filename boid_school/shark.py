0efa8c37cdc388a317c4fefda402eb2f963d56b1
Nathan Snarr <natethegreat209@hotmail.com>
2026-06-29 11:27:33 -0700

    Added empty shark class

---
+++ boid_school/shark.py
@@ -0,0 +1,16 @@
+# Shark
+from pygame import Surface, draw
+
+class Shark:
+    """Chases boids and eats them."""
+
+    def __init__(self):
+        pass
+
+    def update(self, dt: float = 0):
+        """Updates angle and position."""
+        pass
+
+    def draw(self, surface: Surface):
+        """Draws the shark to the surface."""
+        pass
