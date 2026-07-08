fb00af749c24426410170f72071dec35b2b84eb1
Nathan Snarr <natethegreat209@hotmail.com>
2026-07-08 12:52:49 -0700

    Added Shark to __all__

--- boid_school/__init__.py
+++ boid_school/__init__.py
@@ -8,11 +8,13 @@ from .utilities import *
 from .boid import Boid
 from .boid_controller import BoidController
 from .player import Player
+from .shark import Shark
 
 __all__ = ["utilities",
            "Boid",
            "BoidController",
            "Player",
+           "Shark",
            "__version__"]
 
 __version__ = "0.1"
